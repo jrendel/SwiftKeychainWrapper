03d00171c8e7cfc6fc8f1fd1d0cb3f9030376986
Jason Rendel <rendel.jason@gmail.com>
2015-03-24 20:39:08 -0400

    Updated header docs. Option-Clicking on any method name will name show a proper header doc.

--- SwiftKeychainWrapper/KeychainWrapper.swift
+++ SwiftKeychainWrapper/KeychainWrapper.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+
 let SecMatchLimit: String! = kSecMatchLimit as String
 let SecReturnData: String! = kSecReturnData as String
 let SecValueData: String! = kSecValueData as String
@@ -18,19 +19,19 @@ let SecAttrGeneric: String! = kSecAttrGeneric as String
 let SecAttrAccount: String! = kSecAttrAccount as String
 let SecAttrAccessGroup: String! = kSecAttrAccessGroup as String
 
+/// KeychainWrapper is an class to help make Keychain access in Swift more straightforward.
 public class KeychainWrapper {
-   private struct internalVars {
+    // MARK: Private static Properties
+    private struct internalVars {
         static var serviceName: String = ""
         static var accessGroup: String = ""
     }
 
     // MARK: Public Properties
-
-    /*!
-    @var serviceName
-    @abstract Used for the kSecAttrService property to uniquely identify this keychain accessor.
-    @discussion Service Name will default to the app's bundle identifier if it can
-    */
+    
+    /// ServiceName is used for the kSecAttrService property to uniquely identify this keychain accessor. If no service name is specified, KeychainWrapper will default to using the bundleIdentifier. 
+    ///
+    ///This is a static property and only needs to be set once
     public class var serviceName: String {
         get {
             if internalVars.serviceName.isEmpty {
@@ -42,12 +43,12 @@ public class KeychainWrapper {
             internalVars.serviceName = newServiceName
         }
     }
-
-    /*!
-    @var accesGroup
-    @abstract Used for the SecAttrAccessGroup property to identify to which Keychain Sharing Group this entry belongs
-    @discussion Access Group defaults to an empty string and is not used until a valid value is set
-    */
+    
+    /// AccessGroup is used for the kSecAttrAccessGroup property to identify which Keychain Access Group this entry belongs to. This allows you to use the KeychainWrapper with shared keychain access between different applications. 
+    ///
+    /// Access Group defaults to an empty string and is not used until a valid value is set. 
+    ///
+    /// This is a static property and only needs to be set once. To remove the access group property after one has been set, set this to an empty string.
     public class var accessGroup: String {
         get {
             return internalVars.accessGroup
@@ -58,8 +59,13 @@ public class KeychainWrapper {
     }
 
     // MARK: Public Methods
-    public class func hasValueForKey(key: String) -> Bool {
-        var keychainData: NSData? = self.dataForKey(key)
+    
+    /// Checks if keychain data exists for a specified key.
+    ///
+    /// :param: keyName The key to check for.
+    /// :returns: True if a value exists for the key. False otherwise.
+    public class func hasValueForKey(keyName: String) -> Bool {
+        var keychainData: NSData? = self.dataForKey(keyName)
         if let data = keychainData {
             return true
         } else {
@@ -67,7 +73,10 @@ public class KeychainWrapper {
         }
     }
 
-    // MARK: Getting Values
+    /// Returns a string value for a specified key.
+    ///
+    /// :param: keyName The key to lookup data for.
+    /// :returns: The String associated with the key if it exists. If no data exists, or the data found cannot be encoded as a string, returns nil.
     public class func stringForKey(keyName: String) -> String? {
         var keychainData: NSData? = self.dataForKey(keyName)
         var stringValue: String?
@@ -78,6 +87,11 @@ public class KeychainWrapper {
         return stringValue
     }
 
+    
+    /// Returns an object that conforms to NSCoding for a specified key.
+    ///
+    /// :param: keyName The key to lookup data for.
+    /// :returns: The decoded object associated with the key if it exists. If no data exists, or the data found cannot be decoded, returns nil.
     public class func objectForKey(keyName: String) -> NSCoding? {
         let dataValue: NSData? = self.dataForKey(keyName)
 
@@ -90,6 +104,11 @@ public class KeychainWrapper {
         return objectValue;
     }
 
+    
+    /// Returns a NSData object for a specified key.
+    ///
+    /// :param: keyName The key to lookup data for.
+    /// :returns: The NSData object associated with the key if it exists. If no data exists, returns nil.
     public class func dataForKey(keyName: String) -> NSData? {
         var keychainQueryDictionary = self.setupKeychainQueryDictionaryForKey(keyName)
         var result: AnyObject?
@@ -108,7 +127,11 @@ public class KeychainWrapper {
         return status == noErr ? result as? NSData : nil
     }
 
-    // MARK: Setting Values
+    /// Save a String value to the keychain associated with a specified key. If a String value already exists for the given keyname, the string will be overwritten with the new value.
+    ///
+    /// :param: value The String value to save.
+    /// :param: forKey The key to save the String under.
+    /// :returns: True if the save was successful, false otherwise.
     public class func setString(value: String, forKey keyName: String) -> Bool {
         if let data = value.dataUsingEncoding(NSUTF8StringEncoding) {
             return self.setData(data, forKey: keyName)
@@ -117,12 +140,22 @@ public class KeychainWrapper {
         }
     }
 
+    /// Save an NSCoding compliant object to the keychain associated with a specified key. If an object already exists for the given keyname, the object will be overwritten with the new value.
+    ///
+    /// :param: value The NSCoding compliant object to save.
+    /// :param: forKey The key to save the object under.
+    /// :returns: True if the save was successful, false otherwise.
     public class func setObject(value: NSCoding, forKey keyName: String) -> Bool {
         let data = NSKeyedArchiver.archivedDataWithRootObject(value)
 
         return self.setData(data, forKey: keyName)
     }
 
+    /// Save a NSData object to the keychain associated with a specified key. If data already exists for the given keyname, the data will be overwritten with the new value.
+    ///
+    /// :param: value The NSData object to save.
+    /// :param: forKey The key to save the object under.
+    /// :returns: True if the save was successful, false otherwise.
     public class func setData(value: NSData, forKey keyName: String) -> Bool {
         var keychainQueryDictionary: NSMutableDictionary = self.setupKeychainQueryDictionaryForKey(keyName)
 
@@ -142,7 +175,10 @@ public class KeychainWrapper {
         }
     }
 
-    // MARK: Removing Values
+    /// Remove an object associated with a specified key.
+    ///
+    /// :param: keyName The key value to remove data for.
+    /// :returns: True if successful, false otherwise.
     public class func removeObjectForKey(keyName: String) -> Bool {
         let keychainQueryDictionary: NSMutableDictionary = self.setupKeychainQueryDictionaryForKey(keyName)
 
@@ -157,6 +193,8 @@ public class KeychainWrapper {
     }
 
     // MARK: Private Methods
+    
+    /// Update existing data associated with a specified key name. The existing data will be overwritten by the new data
     private class func updateData(value: NSData, forKey keyName: String) -> Bool {
         let keychainQueryDictionary: NSMutableDictionary = self.setupKeychainQueryDictionaryForKey(keyName)
         let updateDictionary = [SecValueData:value]
@@ -171,6 +209,10 @@ public class KeychainWrapper {
         }
     }
 
+    /// Setup the keychain query dictionary used to access the keychain on iOS for a specified key name. Takes into account the Service Name and Access Group if one is set.
+    ///
+    /// :param: keyName The key this query is for
+    /// :returns: A dictionary with all the needed properties setup to access the keychain on iOS
     private class func setupKeychainQueryDictionaryForKey(keyName: String) -> NSMutableDictionary {
         // Setup dictionary to access keychain and specify we are using a generic password (rather than a certificate, internet password, etc)
         var keychainQueryDictionary: NSMutableDictionary = [SecClass:kSecClassGenericPassword]
