816958d080897da92beb2fc08b5e0b077fdaafcc
Forrest Chauvin <forrestchauvin@gmail.com>
2019-08-02 09:39:31 -0400

    Initial support for Swift Package Manager.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "SwiftKeychainWrapper",
+    platforms: [
+        .iOS(.v8),
+    ],
+    products: [
+        .library(
+            name: "SwiftKeychainWrapper",
+            targets: ["SwiftKeychainWrapper"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftKeychainWrapper",
+            dependencies: []),
+        .testTarget(
+            name: "SwiftKeychainWrapperTests",
+            dependencies: ["SwiftKeychainWrapper"]),
+    ]
+)
