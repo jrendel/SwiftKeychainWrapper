0afa366c3aca2b4d0fc22e8c3c2c480f79749de8
Adin Cebic <adin.cebic@klika.ba>
2019-12-23 14:32:37 +0100

    Add package.swift manifest file

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftKeychainWrapper",
+    products: [
+        .library(
+            name: "SwiftKeychainWrapper",
+            targets: ["SwiftKeychainWrapper"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftKeychainWrapper",
+            path: "SwiftKeychainWrapper")
+    ]
+)
